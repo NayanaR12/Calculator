67e8b79eb1727f2a9d30f7e5f8537eff88e63d45
Nayana R <nayanar1299@gmail.com>
2021-06-04 17:42:12 +0530

    commit all

---
+++ calculatortest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Calculatorapp;
+namespace calculatortest
+{
+   
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculator obj = new Calculator();
+        [TestMethod]
+
+        public void TestMethod1()
+        {
+            int actual = obj.add(12, 10);
+            int expected = 22;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
